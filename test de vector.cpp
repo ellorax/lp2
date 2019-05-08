ca901073f9c98348c8831773c4f91f3089a46c88
ellorax <37727126+ellorax@users.noreply.github.com>
2019-05-08 01:18:44 -0500

    Add files via upload

---
+++ test de vector.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include "myvector.h"
+using namespace std;
+int main()
+
+{
+	myvector.vec;
+	int cop;
+	while (1){
+		cin>>cop;
+		if(cop==-1)
+			break;
+		vec.push_back(cop);
+		
+	}
+	vec+=2
+	for(int i=0;i<vec.size();i++)
+		cout<<vec[i]<<" ";
+
+	cout<<endl;
+	return 0
+}
