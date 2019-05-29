31bc38a4aca0c61573dcca1a91699795625c2952
ellorax <37727126+ellorax@users.noreply.github.com>
2019-05-29 08:55:31 -0500

    Update dibujo.h

--- figuras/dibujo.h
+++ figuras/dibujo.h
@@ -8,7 +8,7 @@ class dibujo{
 private:
 
 public:
-    group(int n_figures=0,int n_circules=0,int n_squares=0,int n_triangles=0);
+    dibujo(int n_figures=0,int n_circules=0,int n_squares=0,int n_triangles=0);
     void add_figure(){ n_figures++;}
     void add_circules(){n_circules++;}
     void add_squares(){n_squares++;}
