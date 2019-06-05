61fdefae140c31a62c3717a76420f39a1fb2f01b
ellorax <37727126+ellorax@users.noreply.github.com>
2019-06-05 02:08:16 -0500

    Add files via upload

---
+++ uva/uva 11849.cpp
@@ -0,0 +1,43 @@
+// 11849 - CD
+#include <iostream>
+#include <set>
+using namespace std;
+ 
+int main ()
+{
+  int n; // number of CDs owned by Jack
+  int m; // number of CDs owned by Jill 
+  int result;
+  int temp;
+   
+  while (true)
+  {
+        cin >> n >> m;
+        if (n == 0 && m == 0)
+           break;
+         
+        set<int> myset;
+ 
+        result = 0;
+         
+        for (int i = 0; i < n; i++)
+        {
+            cin >> temp;
+            myset.insert(temp);
+        }
+         
+        for (int i = 0; i < m; i++)
+        {
+            cin >> temp;
+            int tempSize = myset.size();
+            myset.insert(temp);
+            if (tempSize == myset.size())
+               result++;
+        }
+         
+        cout << result << "\n";
+             
+  }
+   
+  return 0;
+}
